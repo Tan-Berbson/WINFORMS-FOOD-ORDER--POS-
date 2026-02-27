6d00adf5207419b4455e3724efd50be47d41e62c
Tan-Berbson <berbsontan01@gmail.com>
2026-02-27 09:54:31 +0800

    # create frm_reciept

---
+++ WINFORMS-FOOD-ORDER-(POS)/frm_reciept.Designer.cs
@@ -0,0 +1,469 @@
+﻿namespace WINFORMS_FOOD_ORDER__POS_
+{
+    partial class frm_reciept
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            txt_managername = new TextBox();
+            txt_cashiername = new TextBox();
+            pictureboxlogo = new PictureBox();
+            label3 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel5 = new Panel();
+            panel4 = new Panel();
+            panel6 = new Panel();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button12 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureboxlogo).BeginInit();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel5.SuspendLayout();
+            panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(txt_managername);
+            panel1.Controls.Add(txt_cashiername);
+            panel1.Controls.Add(pictureboxlogo);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(2, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(618, 57);
+            panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(349, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 15);
+            label1.TabIndex = 17;
+            label1.Text = "Cashier Name:";
+            // 
+            // txt_managername
+            // 
+            txt_managername.Font = new Font("Segoe UI", 12F);
+            txt_managername.Location = new Point(207, 12);
+            txt_managername.Name = "txt_managername";
+            txt_managername.ReadOnly = true;
+            txt_managername.Size = new Size(136, 29);
+            txt_managername.TabIndex = 16;
+            // 
+            // txt_cashiername
+            // 
+            txt_cashiername.Font = new Font("Segoe UI", 12F);
+            txt_cashiername.Location = new Point(439, 12);
+            txt_cashiername.Name = "txt_cashiername";
+            txt_cashiername.ReadOnly = true;
+            txt_cashiername.Size = new Size(173, 29);
+            txt_cashiername.TabIndex = 18;
+            // 
+            // pictureboxlogo
+            // 
+            pictureboxlogo.Location = new Point(3, 4);
+            pictureboxlogo.Name = "pictureboxlogo";
+            pictureboxlogo.Size = new Size(100, 50);
+            pictureboxlogo.TabIndex = 14;
+            pictureboxlogo.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(109, 20);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 15;
+            label3.Text = "Manager Name:";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button11);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(panel6);
+            panel2.Location = new Point(8, 194);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(327, 463);
+            panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(listBox1);
+            panel3.Controls.Add(panel9);
+            panel3.Location = new Point(341, 194);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(279, 463);
+            panel3.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button12);
+            panel5.Location = new Point(5, 663);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(615, 45);
+            panel5.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label6);
+            panel4.Controls.Add(label5);
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(label2);
+            panel4.Controls.Add(textBox5);
+            panel4.Controls.Add(textBox4);
+            panel4.Controls.Add(textBox3);
+            panel4.Controls.Add(textBox2);
+            panel4.Location = new Point(5, 65);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(615, 123);
+            panel4.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(button10);
+            panel6.Controls.Add(button7);
+            panel6.Controls.Add(button8);
+            panel6.Controls.Add(button9);
+            panel6.Controls.Add(button4);
+            panel6.Controls.Add(button5);
+            panel6.Controls.Add(button6);
+            panel6.Controls.Add(button3);
+            panel6.Controls.Add(button2);
+            panel6.Controls.Add(button1);
+            panel6.Location = new Point(3, 169);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(320, 283);
+            panel6.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            panel8.Location = new Point(8, 194);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(327, 29);
+            panel8.TabIndex = 1;
+            // 
+            // panel9
+            // 
+            panel9.Location = new Point(0, 0);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(273, 29);
+            panel9.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(3, 28);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(270, 424);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 24F);
+            textBox1.Location = new Point(3, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(324, 50);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(14, 16);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 60);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(112, 16);
+            button2.Name = "button2";
+            button2.Size = new Size(92, 60);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(210, 16);
+            button3.Name = "button3";
+            button3.Size = new Size(92, 60);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(210, 82);
+            button4.Name = "button4";
+            button4.Size = new Size(92, 60);
+            button4.TabIndex = 6;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(112, 82);
+            button5.Name = "button5";
+            button5.Size = new Size(92, 60);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(14, 82);
+            button6.Name = "button6";
+            button6.Size = new Size(92, 60);
+            button6.TabIndex = 4;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(210, 148);
+            button7.Name = "button7";
+            button7.Size = new Size(92, 60);
+            button7.TabIndex = 9;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(112, 148);
+            button8.Name = "button8";
+            button8.Size = new Size(92, 60);
+            button8.TabIndex = 8;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(14, 148);
+            button9.Name = "button9";
+            button9.Size = new Size(92, 60);
+            button9.TabIndex = 7;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(112, 214);
+            button10.Name = "button10";
+            button10.Size = new Size(92, 60);
+            button10.TabIndex = 10;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(7, 103);
+            button11.Name = "button11";
+            button11.Size = new Size(320, 60);
+            button11.TabIndex = 11;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(137, 21);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(158, 29);
+            textBox2.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F);
+            textBox3.Location = new Point(137, 70);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(158, 29);
+            textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F);
+            textBox4.Location = new Point(436, 21);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(151, 29);
+            textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F);
+            textBox5.Location = new Point(436, 70);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(151, 29);
+            textBox5.TabIndex = 22;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(5, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 21);
+            label2.TabIndex = 23;
+            label2.Text = "Customer Name:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(51, 73);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 21);
+            label4.TabIndex = 24;
+            label4.Text = "Order Id#:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(339, 29);
+            label5.Name = "label5";
+            label5.Size = new Size(90, 21);
+            label5.TabIndex = 25;
+            label5.Text = "Order Total:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.Location = new Point(301, 78);
+            label6.Name = "label6";
+            label6.Size = new Size(128, 21);
+            label6.TabIndex = 26;
+            label6.Text = "Payment Method";
+            // 
+            // button12
+            // 
+            button12.Location = new Point(3, 3);
+            button12.Name = "button12";
+            button12.Size = new Size(606, 39);
+            button12.TabIndex = 11;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // frm_reciept
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(625, 710);
+            Controls.Add(panel8);
+            Controls.Add(panel4);
+            Controls.Add(panel5);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "frm_reciept";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frm_reciept";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureboxlogo).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel6.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox txt_managername;
+        private TextBox txt_cashiername;
+        private PictureBox pictureboxlogo;
+        private Label label3;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel5;
+        private TextBox textBox1;
+        private Panel panel6;
+        private Button button1;
+        private ListBox listBox1;
+        private Panel panel4;
+        private Panel panel8;
+        private Panel panel9;
+        private Button button11;
+        private Button button10;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button3;
+        private Button button2;
+        private Label label2;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Button button12;
+    }
+}
