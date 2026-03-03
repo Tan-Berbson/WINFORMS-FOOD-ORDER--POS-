e71b84f2c284ef161592d6db7ef822ceba455a80
Tan-Berbson <berbsontan01@gmail.com>
2026-03-03 08:13:53 +0800

    # add lock in log in  if the user enter invalid credentials

--- WINFORMS-FOOD-ORDER-(POS)/frm_login.cs
+++ WINFORMS-FOOD-ORDER-(POS)/frm_login.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 using static WINFORMS_FOOD_ORDER__POS_.Class1;
 namespace WINFORMS_FOOD_ORDER__POS_
@@ -13,21 +14,37 @@ namespace WINFORMS_FOOD_ORDER__POS_
     public partial class frm_login : Form
     {
         auth login = new auth();
-       
+        private int loginAttempts = 0; 
+        private const int MaxLoginAttempts = 6; 
+        private const int LockoutDuration = 60; 
+        private System.Windows.Forms.Timer lockoutTimer;
+        private DateTime lockoutEndTime; 
+
         public frm_login()
         {
             InitializeComponent();
             txt_password.UseSystemPasswordChar = true;
+            lockoutTimer = new System.Windows.Forms.Timer();
+            lockoutTimer.Interval = 1000; 
+            lockoutTimer.Tick += LockoutTimer_Tick;
         }
 
         private void btn_login_Click(object sender, EventArgs e)
         {
+            // Check if the user is locked out
+            if (lockoutTimer.Enabled)
+            {
+                TimeSpan remainingTime = lockoutEndTime - DateTime.Now;
+                MessageBox.Show($"Account is locked. Please try again in {remainingTime.Seconds} seconds.", "Account Locked", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
             if (login.loginadmin(txt_username.Text, txt_password.Text))
             {
                 MessageBox.Show("Log In Successfully Admin", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 frm_admindashboard f = new frm_admindashboard(txt_username.Text);
                 f.Show();
                 this.Hide();
+                loginAttempts = 0; 
             }
             else if (login.logincashier(txt_username.Text, txt_password.Text))
             {
@@ -35,6 +52,7 @@ namespace WINFORMS_FOOD_ORDER__POS_
                 frm_cashierDashboard f = new frm_cashierDashboard(txt_username.Text);
                 f.Show();
                 this.Hide();
+                loginAttempts = 0; 
 
             }
             else if (string.IsNullOrWhiteSpace(txt_username.Text) || string.IsNullOrWhiteSpace(txt_password.Text))
@@ -47,9 +65,31 @@ namespace WINFORMS_FOOD_ORDER__POS_
             }
             else
             {
-                MessageBox.Show("Log In Failed Check Your Username and Password", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                loginAttempts++;
+                if (loginAttempts >= MaxLoginAttempts)
+                {
+                    // Lock the account for 60 seconds
+                    lockoutEndTime = DateTime.Now.AddSeconds(LockoutDuration);
+                    lockoutTimer.Start();
+                    MessageBox.Show($"Too many failed attempts. Account is locked for {LockoutDuration} seconds.", "Account Locked", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show($"Incorrect Username or Password. Attempts left: {MaxLoginAttempts - loginAttempts}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
+        private void LockoutTimer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= lockoutEndTime)
+            {
+                lockoutTimer.Stop();
+                loginAttempts = 0; 
+                MessageBox.Show("You can now try logging in again.", "Lockout Ended", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
 
         private void btn_signup_Click(object sender, EventArgs e)
         {
