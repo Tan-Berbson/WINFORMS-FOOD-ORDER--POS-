72b358a46ab26faf02840442bc5271e2348845cc
Tan-Berbson <berbsontan01@gmail.com>
2026-03-06 09:39:26 +0800

    # add nugets charts

---
+++ WINFORMS-FOOD-ORDER-(POS)/frm_montlysales.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WINFORMS_FOOD_ORDER__POS_
+{
+    partial class frm_montlysales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_montlysales));
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            txt_username = new TextBox();
+            btn_logout = new Button();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(86, 127);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(442, 300);
+            chart1.TabIndex = 0;
+            chart1.Text = "chart1";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(52, 94, 104);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(txt_username);
+            panel1.Controls.Add(btn_logout);
+            panel1.Location = new Point(3, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1099, 57);
+            panel1.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(74, 50);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(102, 20);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Admin Name";
+            // 
+            // txt_username
+            // 
+            txt_username.Font = new Font("Segoe UI", 12F);
+            txt_username.Location = new Point(186, 12);
+            txt_username.Name = "txt_username";
+            txt_username.ReadOnly = true;
+            txt_username.Size = new Size(173, 29);
+            txt_username.TabIndex = 9;
+            // 
+            // btn_logout
+            // 
+            btn_logout.Location = new Point(965, 8);
+            btn_logout.Name = "btn_logout";
+            btn_logout.Size = new Size(122, 38);
+            btn_logout.TabIndex = 9;
+            btn_logout.Text = "Log Out";
+            btn_logout.UseVisualStyleBackColor = true;
+            // 
+            // frm_montlysales
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1099, 806);
+            Controls.Add(panel1);
+            Controls.Add(chart1);
+            Name = "frm_montlysales";
+            Text = "frm_montlysales";
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private TextBox txt_username;
+        private Button btn_logout;
+    }
+}
